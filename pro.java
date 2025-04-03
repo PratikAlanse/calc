5ab5c8ce421845e6f461fd67bf96a4a1f749649f
Pratik <asus@DESKTOP-PVSIMIE.>
2025-04-03 15:13:22 +0530

    Program commited 1

---
+++ pro.java
@@ -0,0 +1,14 @@
+import java.lang.object;
+import java.util.*;
+class Add {
+	public static void main(String [] arg)
+	{
+		Scanncer sc = new Scanner(System.in);
+		system.out.print("Enter two number:");
+		a1=sc.nextInt();
+		a2=sc.nextInt();
+		int sum=0;
+		sum = a1+a2;
+		system.out.print(sum);
+	}
+}
